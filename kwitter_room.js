3feee44893bc5b375d309eb07ddd8be25b079ff3
vardaraj-choudhary <84618312+vardaraj-choudhary@users.noreply.github.com>
2022-04-01 10:55:38 +0530

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,7 @@
+
+function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
+       Room_names = childKey;
+      
+  
+      });});}
+getData();
